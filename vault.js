e5c51f72d339a4f36a200d335f9ba67189e2b0a4
matukai <matt.tanji@gmail.com>
2018-01-02 18:23:06 -1000

    vault exercise

--- vault.js
+++ vault.js
@@ -1,4 +1,22 @@
 'use strict';
 module.exports = function() {
   
+  let obj = {};
+
+  let setValue = function setValue (key,value){
+    obj[key] = value;
+  }
+  let getValue = function getValue (key){
+    if(key === undefined || !obj.hasOwnProperty(key)){
+      return null;
+    }else{
+      return obj[key];
+    }
+  }
+
+return{
+  setValue: setValue,
+  getValue: getValue
+}
+
 };
